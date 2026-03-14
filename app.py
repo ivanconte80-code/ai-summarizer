6cf0b45d4469a14c4df2abcd7d5f4eeb5b1cd56a
ivanconte80-code <ivanconte80@gmail.com>
2026-03-14 12:12:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,8 @@ def home():
 
     return render_template("index.html", risultato=risultato)
 
-app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
